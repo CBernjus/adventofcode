b4a9a6cd41b51a928cdfa95b6b5b29ac36b1bb4a
Christopher Bernjus <christopher.bernjus@stud.tu-darmstadt.de>
2020-12-07 11:13:15 +0100

    feat(2020): day_6 (python)

---
+++ 2020/python/day_6.py
@@ -0,0 +1,138 @@
+# ---------------------------
+# Advent of Code 2020 - Day 6
+# Part 1: Custom Customs
+# ---------------------------
+
+# As your flight approaches the regional airport where you'll switch to a much larger plane, customs declaration forms are distributed to the passengers.
+
+# The form asks a series of 26 yes-or-no questions marked a through z. All you need to do is identify the questions for which anyone in your group answers "yes". Since your group is just you, this doesn't take very long.
+
+# However, the person sitting next to you seems to be experiencing a language barrier and asks if you can help. For each of the people in their group, you write down the questions for which they answer "yes", one per line. For example:
+
+# abcx
+# abcy
+# abcz
+
+# In this group, there are 6 questions to which anyone answered "yes": a, b, c, x, y, and z. (Duplicate answers to the same question don't count extra; each question counts at most once.)
+
+# Another group asks for your help, then another, and eventually you've collected answers from every group on the plane (your puzzle input). Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line. For example:
+
+# abc
+
+# a
+# b
+# c
+
+# ab
+# ac
+
+# a
+# a
+# a
+# a
+
+# b
+
+# This list represents answers from five groups:
+
+# - The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+# - The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+# - The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+# - The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+# - The last group contains one person who answered "yes" to only 1 question, b.
+
+# In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+
+# For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+
+import os
+
+f = open(os.path.dirname(__file__) + "/../inputs/input_6.txt")
+
+groups = f.read().split('\n\n')
+
+#groups = 'abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb'.split('\n\n')
+#groups = 'pkitmzfu\npuikeqf\n\nyafwqrdhpn\nbxoevlstjm\n\niohkjpefsmvadgcu\nplhbiucsgjtwaovfm\ngjmpuvifchqasko\nmsvxhogjiuafcp\nicvoujazphgmfs'.split('\n\n')
+groups = [group.split('\n') for group in groups]
+
+def count_questions_of_any(group):
+    questions = []
+    for person in group:
+        for q in person:
+            if q not in questions:
+                questions.append(q)
+    return len(questions)
+
+def sum_any_questions(groups):
+    questions = [count_questions_of_any(group) for group in groups]
+    return sum(questions)
+
+print("2020 - Day 6 - Part 1")
+print(sum_any_questions(groups))
+# => 6170
+#    ====
+
+# ---------------------------
+# Advent of Code 2020 - Day 6
+# Part 2: Custom Customs
+# ---------------------------
+
+# As you finish the last group's customs declaration, you notice that you
+# misread one word in the instructions:
+
+# You don't need to identify the questions to which anyone answered "yes";
+# you need to identify the questions to which everyone answered "yes"!
+
+# Using the same example as above:
+
+# abc
+
+# a
+# b
+# c
+
+# ab
+# ac
+
+# a
+# a
+# a
+# a
+
+# b
+
+# This list represents answers from five groups:
+
+# - In the first group, everyone (all 1 person) answered "yes" to 3
+#   questions: a, b, and c.
+# - In the second group, there is no question to which everyone answered
+#   "yes".
+# - In the third group, everyone answered yes to only 1 question, a. Since
+#   some people did not answer "yes" to b or c, they don't count.
+# - In the fourth group, everyone answered yes to only 1 question, a.
+# - In the fifth group, everyone (all 1 person) answered "yes" to 1
+#   question, b.
+
+# In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+# For each group, count the number of questions to which everyone answered
+# "yes". What is the sum of those counts?
+
+def count_questions_of_all(group):
+    questions = list(group[0])
+    for person in group[1:]:
+        toRemove = []
+        for q in questions:
+            if q not in person:
+                toRemove.append(q)
+        questions = [q for q in questions if q not in toRemove]
+    return len(questions)
+
+def sum_all_questions(groups):
+    questions = [count_questions_of_all(group) for group in groups]
+    return sum(questions)
+
+print("\n2020 - Day 6 - Part 2")
+print(sum_all_questions(groups))
+# => 2947
+#    ====
