7d0e551fd540f211291cfc536ab484030a9b911a
Christopher Bernjus <christopher.bernjus@stud.tu-darmstadt.de>
2022-04-01 01:23:59 +0200

    feat(2021): create day template

---
+++ 2021/python/day.py
@@ -0,0 +1,39 @@
+# ---------------------------
+# Advent of Code 2021 - Day X
+# Part 1: XXX
+# ---------------------------
+
+# 
+
+from __future__ import annotations
+import os
+from typing import Dict, List, Tuple
+
+# with open(os.path.dirname(__file__) + "/../examples/example_2.txt") as f:
+with open(os.path.dirname(__file__) + "/../inputs/input_2.txt") as f:
+
+    instructions = list(map(int, f.readlines()))
+
+    print("2021 - Day X - Part 1")
+    print("question")
+    # => 
+    #    =======
+
+print()
+
+# ---------------------------
+# Advent of Code 2021 - Day X
+# Part 2: XXX
+# ---------------------------
+
+# 
+
+# with open(os.path.dirname(__file__) + "/../examples/example_2.txt") as f:
+with open(os.path.dirname(__file__) + "/../inputs/input_2.txt") as f:
+
+    instructions = list(map(int, f.readlines()))
+
+    print("2021 - Day X - Part 2")
+    print("XXX")
+    # => 
+    #    ==========
