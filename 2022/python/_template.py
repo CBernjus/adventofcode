679d2ae11406dab5ffdde97deb5b3f838bc5a9bf
Christopher Bernjus <github@bernjus.de>
2022-12-01 18:01:51 +0100

    feat(2022): day_1 (python)

---
+++ 2022/python/_template.py
@@ -0,0 +1,39 @@
+# ---------------------------
+# Advent of Code 2022 - Day X
+# Part 1: XXX
+# ---------------------------
+
+#
+
+from __future__ import annotations
+import os
+from typing import Dict, List, Tuple
+
+# with open(os.path.dirname(__file__) + "/../examples/example_X.txt") as f:
+with open(os.path.dirname(__file__) + "/../inputs/input_X.txt") as f:
+
+    arr = list(map(int, f.readlines()))
+
+    print("2022 - Day X - Part 1")
+    print("question")
+    # =>
+    #    =======
+
+print()
+
+# ---------------------------
+# Advent of Code 2022 - Day X
+# Part 2: XXX
+# ---------------------------
+
+#
+
+# with open(os.path.dirname(__file__) + "/../examples/example_X.txt") as f:
+with open(os.path.dirname(__file__) + "/../inputs/input_X.txt") as f:
+
+    arr = list(map(int, f.readlines()))
+
+    print("2022 - Day X - Part 2")
+    print("XXX")
+    # =>
+    #    ==========
