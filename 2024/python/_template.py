fecad29aabf11f72b4dd795b4db4f9ca6bcf621b
Christopher Bernjus <christopher@bernjus.de>
2024-12-02 00:27:10 +0100

    feat(2024): day_1 (python)

---
+++ 2024/python/_template.py
@@ -0,0 +1,40 @@
+DAY = 1
+# ---------------------------
+# Advent of Code 2024 - Day X
+# Part 1: XXX
+# ---------------------------
+
+#
+
+from __future__ import annotations
+import os
+from typing import Dict, List, Tuple
+
+with open(os.path.dirname(__file__) + f"/../examples/example_{DAY}.txt") as f:
+# with open(os.path.dirname(__file__) + f"/../inputs/input_{DAY}.txt") as f:
+
+    arr = list(map(int, f.readlines()))
+
+    print(f"2024 - Day {DAY} - Part 1")
+    print("question")
+    # =>
+    #    =======
+
+print()
+
+# ---------------------------
+# Advent of Code 2024 - Day X
+# Part 2: XXX
+# ---------------------------
+
+#
+
+with open(os.path.dirname(__file__) + f"/../examples/example_{DAY}.txt") as f:
+# with open(os.path.dirname(__file__) + f"/../inputs/input_{DAY}.txt") as f:
+
+    arr = list(map(int, f.readlines()))
+
+    print(f"2024 - Day {DAY} - Part 2")
+    print("question")
+    # =>
+    #    ==========
